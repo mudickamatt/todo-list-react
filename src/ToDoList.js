3773223c3e69d89a82f6a90a9c26f68d1bb9cfda
Matthew Mudicka <mudickamatt@gmail.com>
2020-02-08 20:14:40 -0500

    Added todo item components

---
+++ src/ToDoList.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import ToDoItem from "./ToDoItem";
+
+export default class ToDoList extends Component {
+  render() {
+    const { items, clearList, handleDelete } = this.props;
+    return (
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">todo list</h3>
+        {items.map(item => {
+          return (
+            <ToDoItem
+              key={item.id}
+              title={item.title}
+              handleDelete={() => handleDelete(item.id)}
+            />
+          );
+        })}
+
+        <button
+          type="button"
+          className="btn btn-danger btn-block text-capitalize mt-5"
+          onClick={clearList}
+        >
+          clear list
+        </button>
+      </ul>
+    );
+  }
+}
